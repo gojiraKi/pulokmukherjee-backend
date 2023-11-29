ef9062ade71aef25f473e57ed6bfd2939054570f
Phalkishwor <sign.up.email.for.social.media@gmail.com>
2023-11-29 11:46:59 +0530

    initial commit

---
+++ api/controllers/SiteController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace api\controllers;
+
+use common\models\LoginForm;
+use Yii;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => \yii\web\ErrorAction::class,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
