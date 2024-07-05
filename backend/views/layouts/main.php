318e7b0780777f0f70f2c14832a1c503c59659b7
Phalkishwor <sign.up.email.for.social.media@gmail.com>
2024-07-05 10:47:28 +0530

    backend layout main update. navbar moved. modal added

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -8,74 +8,76 @@ use common\widgets\Alert;
 use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
 use yii\helpers\Url;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+use kartik\bs5dropdown\Dropdown;
 
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
-    
+
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <!-- include summernote css/js -->
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-    
+
 </head>
-<body class="d-flex flex-column h-100 bg-white">
+
+<!-- <body class="d-flex flex-column h-100 bg-white"> -->
+<body class="d-flex flex-column h-100 bg-brownish">
 <?php $this->beginBody() ?>
 
-<div class="row g-0">
-    <div class="col-lg-2">
-        <?= $this->render('sidenav') ?>
-    </div>
-    <div class="col-lg-10">
-        <?= $this->render('headernav') ?>
-        <main role="main" class="flex-shrink-0">
-            <div class="container">
-                <?= Breadcrumbs::widget([
-                    'homeLink' => [
-                        'label' => '<i class="fa fa-home"></i> ' . Html::encode(Yii::t('yii', 'Home')),
-                        'url' => Url::toRoute(['default/index']),
-                        'encode' => false,
-                    ],
-                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-                    'options' => ['class' => 'p-2 bg-light border rounded shadow-sm']
-                ]) ?>
+<header>
+    <?= $this->render('navbar') ?>
+</header>
 
-                <?= Alert::widget() ?>
-                <?= $content ?>
-            </div>
-        </main>
+<main role="main" class="flex-shrink-0 pt-5">
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'homeLink' => [
+                'label' => '<i class="fa fa-home"></i> ' . Html::encode(Yii::t('yii', 'Home')),
+                'url' => Url::toRoute(['default/index']),
+                'encode' => false,
+            ],
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            'options' => ['class' => 'p-2 bg-light border rounded shadow-sm']
+        ]) ?>
 
-        <footer class="footer mt-auto py-3 text-muted bg-white">
-            <div class="container">
-                <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-                <p class="float-end">Powered by: <a href='https://whitesoul-ds.co.in/' target='_blank'>WhiteSoul</a></p>
-            </div>
-        </footer>
+        <?= Alert::widget() ?>
+        <?= $content ?>
     </div>
-</div>
+</main>
+
+<footer class="footer mt-auto py-3 text-muted bg-white">
+    <div class="container">
+        <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+        <p class="float-end">Powered by: <a href='https://whitesoul-ds.co.in/' target='_blank'>WhiteSoul</a></p>
+    </div>
+</footer>
 
 
 
 <?php $this->endBody() ?>
 <?php
-    \yii\bootstrap5\Modal::begin([
-        'headerOptions' => ['id' => 'modalHeader'],
-        'id' => 'modalPL',
-        'size' => 'modal-lg',
-        //keeps from closing modal with esc key or by clicking out of the modal.
-        // user must click cancel or X to close
-        'clientOptions' => ['backdrop' => 'static', 'keyboard' => FALSE],
-        "footer"=>"",
-    ]);
-    echo "<div id='modalContent'><div style='text-align:center'><img src='" . Url::home() . "img/Spinning_gear.gif'></div></div>";
-    \yii\bootstrap5\Modal::end();
+\yii\bootstrap5\Modal::begin([
+    'headerOptions' => ['id' => 'modalHeader'],
+    'id' => 'modalPL',
+    'size' => 'modal-lg',
+    //keeps from closing modal with esc key or by clicking out of the modal.
+    // user must click cancel or X to close
+    'clientOptions' => ['backdrop' => 'static', 'keyboard' => FALSE],
+    "footer" => "",
+]);
+echo "<div id='modalContent'><div style='text-align:center'><img src='" . Url::home() . "img/Spinning_gear.gif'></div></div>";
+\yii\bootstrap5\Modal::end();
 ?>
 
 <script>
@@ -96,5 +98,6 @@ AppAsset::register($this);
 </script>
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 </body>
+
 </html>
 <?php $this->endPage();
---
+++ backend/views/layouts/navbar.php
@@ -0,0 +1,99 @@
+<?php
+use yii\bootstrap5\Html;
+use yii\helpers\Url;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+use kartik\bs5dropdown\Dropdown;
+
+?>
+<?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandOptions' => ['class' => 'p-0'],
+        'options' => ['class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top']
+    ]);
+    echo Nav::widget([
+        'items' => [
+            ['label' => 'Home', 'url' => ['/site/index']],
+            [
+                'label' => 'Profile',
+                'items' => [
+                    ['label' => 'About', 'url' => Url::toRoute(['about/index'])],
+                    ['label' => 'Research', 'url' => '#'],
+                    ['label' => 'Lectures', 'url' => Url::toRoute(['lecture/index'])],
+                ],
+            ],
+            ['label' => 'Outreach & Reports', 'url' => Url::toRoute(['outreach-activity/index'])],
+            [
+                'label' => 'Publication',
+                'items' => [
+                    ['label' => 'Research Papers', 'url' => Url::toRoute(['publications/index'])],
+                    ['label' => 'Books', 'url' => Url::toRoute(['book/index'])]
+                ],
+            ],
+            [
+                'label' => 'Achivement',
+                'items' => [
+                    ['label' => 'Honors and Awards', 'url' => Url::toRoute(['about/index'])],
+                    ['label' => 'Government and Industry Projects', 'url' => '#'],
+                    ['label' => 'PhD Projects', 'url' => Url::toRoute(['lecture/index'])],
+                ],
+            ],
+            [
+                'label' => 'Activities',
+                'items' => [
+                    ['label' => 'Professional Activities', 'url' => Url::toRoute(['activity/index'])],
+                    ['label' => 'Conference Seminar', 'url' => Url::toRoute(['conference-and-seminar/index'])],
+                    // ['label' => 'Outreach Activity', 'url' => Url::toRoute(['outreach-activity/index'])],
+                ],
+            ],
+            [
+                'label' => 'More',
+                'items' => [
+                    ['label' => 'Gallery', 'url' => Url::toRoute(['gallery/index'])],
+                    ['label' => 'My Family', 'url' => Url::toRoute(['my-family/index'])]
+                ],
+            ],
+            ['label' => 'Recent Highlights', 'url' => Url::toRoute(['recent-highlight/index'])],
+            [
+                'label' => 'Misc',
+                'items' => [
+                    ['label' => 'Media', 'url' => Url::toRoute(['media/index'])]
+                ],
+            ]
+        ],
+        'dropdownClass' => Dropdown::class, // use the custom dropdown
+        'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
+    ]);
+    NavBar::end();
+
+    // Configuring the Bootstrap 5 Dropdown widget
+    // echo Html::tag('span', 'Dropdown Span', [
+    //     'id' => 'dropdownMenuButton',
+    //     'class' => 'btn btn-link text-info dropdown-toggle',
+    //     'data-bs-toggle' => 'dropdown',
+    //     'aria-haspopup' => 'true',
+    //     'aria-expanded' => 'false'
+    // ]);
+    // echo Dropdown::widget([
+    //     'items' => [
+    //         ['label' => 'Section 1', 'url' => '/'],
+    //         ['label' => 'Section 2', 'url' => '#'],
+    //         [
+    //             'label' => 'Section 3',
+    //             'items' => [
+    //                 ['label' => 'Section 3.1', 'url' => '/'],
+    //                 ['label' => 'Section 3.2', 'url' => '#'],
+    //                 [
+    //                     'label' => 'Section 3.3',
+    //                     'items' => [
+    //                         ['label' => 'Section 3.3.1', 'url' => '/'],
+    //                         ['label' => 'Section 3.3.2', 'url' => '#'],
+    //                     ],
+    //                 ],
+    //             ],
+    //         ],
+    //     ],
+    //     'options' => ['aria-labelledby' => 'dropdownMenuButton']
+    // ]);
+?>
